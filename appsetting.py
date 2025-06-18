3bd00a7c815111a27f8d47409a6075f6b9b57fb5
Boomtrptw <theerapong.t@vri.co.th>
2025-06-18 18:20:01 +0700

    เอาอีก

--- appsetting.py
+++ appsetting.py
@@ -1,3 +1,9 @@
-protocol = "https://"
-host = "127.0.0.1"
-port = 1704
+import os
+
+protocol = "http://"
+host = os.getenv("RENDER_EXTERNAL_HOSTNAME", "127.0.0.1")  # ใช้ค่าจาก Environment Variable ถ้ามี
+port = os.getenv("PORT", 1704)  # หรือค่าพอร์ตที่กำหนดใน Render
+
+base_url = f"{protocol}{host}:{port}"
+
+print(base_url)  # สำหรับตรวจสอบการตั้งค่า
